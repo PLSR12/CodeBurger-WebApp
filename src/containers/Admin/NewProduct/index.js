bf3de31369789a4548a6b328bb917c3d5aece1dd
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-09-20 20:47:06 -0300

    [*] updated page

--- src/containers/Admin/NewProduct/index.js
+++ src/containers/Admin/NewProduct/index.js
@@ -1,15 +1,15 @@
 import { yupResolver } from '@hookform/resolvers/yup'
 import CloudUploadIcon from '@mui/icons-material/CloudUpload'
 import React, { useEffect, useState } from 'react'
-import { Controller, useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import { useHistory } from 'react-router-dom'
-import ReactSelect from 'react-select'
 import { toast } from 'react-toastify'
 import * as Yup from 'yup'
 
 import api from '../../../services/api'
 
 import * as Atoms from '../../../components/Atoms'
+import { maskCurrencyInput } from '../../../utils/maskCurrencyInput'
 
 import * as S from './styles'
 
@@ -22,8 +22,8 @@ function NewProduct() {
     const productDataFormData = new FormData()
 
     productDataFormData.append('name', data.name)
-    productDataFormData.append('price', data.price)
-    productDataFormData.append('category_id', data.category.id)
+    productDataFormData.append('price', customParseFloat(data.price))
+    productDataFormData.append('category_id', data.category)
     productDataFormData.append('offer', data.offer)
     productDataFormData.append('file', data.file[0])
 
@@ -37,10 +37,14 @@ function NewProduct() {
     }, 2000)
   }
 
+  const customParseFloat = (value) => {
+    return parseFloat(value.replace(/\D/g, '')) / 100
+  }
+
   const schema = Yup.object().shape({
-    name: Yup.string().required('O name é obrigatório'),
+    name: Yup.string().required('O nome é obrigatório'),
     price: Yup.string().required('O preço é obrigátoria'),
-    category: Yup.object().required('Escolha uma categoria'),
+    category: Yup.string().required('Escolha uma categoria'),
     file: Yup.mixed().test('required', 'Carregue uma imagem', (value) => {
       return value && value.length > 0
     }),
@@ -57,67 +61,79 @@ function NewProduct() {
     async function loadCategories() {
       const { data } = await api.get('categories')
 
-      setCategories(data)
+      data.map((category) => {
+        const categroyId = category.id
+        const categoryLabel = category.name
+
+        const categoryOption = {
+          id: `${categroyId}`,
+          label: `${categoryLabel}`,
+        }
+
+        setCategories((prevState) => [...prevState, categoryOption])
+      })
     }
     loadCategories()
   }, [])
 
   return (
     <S.Container>
-      <form noValidate onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <S.Label>Nome:</S.Label>
-          <S.Input type="text" {...register('name')} />
-          <Atoms.ErrorMessage>{errors.name?.message}</Atoms.ErrorMessage>
-        </div>
-        <div>
-          <S.Label> Preço </S.Label>
-          <S.Input type="number" {...register('price')} />
-          <Atoms.ErrorMessage>{errors.price?.message}</Atoms.ErrorMessage>
-        </div>
-        <div>
-          <S.LabelUpload>
-            {fileName || (
-              <>
-                <CloudUploadIcon />
-                Caregue a imagem do produto
-              </>
-            )}
-            <input
-              type="file"
-              accept="image/png , image/jpeg"
-              {...register('file')}
-              onChange={(value) => {
-                setFileName(value.target.files[0]?.name)
-              }}
+      <Atoms.Box>
+        <form noValidate onSubmit={handleSubmit(onSubmit)}>
+          <Atoms.InputComponent
+            type="text"
+            label="Nome"
+            {...register('name')}
+            error={errors.name}
+            style={{ marginBottom: '10px' }}
+            placeholder="Digite o Nome:"
+          />
+
+          <Atoms.InputComponent
+            type="text"
+            label="Preço"
+            min={0}
+            {...register('price')}
+            error={errors.price}
+            onInput={maskCurrencyInput}
+            placeholder="Digite o Preço:"
+          />
+          <div>
+            <S.LabelUpload>
+              {fileName || (
+                <>
+                  <CloudUploadIcon />
+                  Caregue a imagem do produto
+                </>
+              )}
+
+              <input
+                type="file"
+                accept="image/png , image/jpeg"
+                {...register('file')}
+                onChange={(value) => {
+                  setFileName(value.target.files[0]?.name)
+                }}
+              />
+            </S.LabelUpload>
+            <Atoms.ErrorMessage>{errors.file?.message}</Atoms.ErrorMessage>
+          </div>
+          <div>
+            <Atoms.SelectComponent
+              label={'Selecione uma Categoria'}
+              options={categories}
+              {...register('category')}
+              error={errors.category}
             />
-          </S.LabelUpload>
-          <Atoms.ErrorMessage>{errors.file?.message}</Atoms.ErrorMessage>
-        </div>
-        <div>
-          <Controller
-            name="category"
-            control={control}
-            render={({ field }) => {
-              return (
-                <ReactSelect
-                  {...field}
-                  options={categories}
-                  getOptionLabel={(cat) => cat.name}
-                  getOptionValue={(cat) => cat.id}
-                  placeholder="Escolha a categoria"
-                />
-              )
-            }}
-          ></Controller>
-          <Atoms.ErrorMessage>{errors.category?.message}</Atoms.ErrorMessage>
-        </div>
-        <S.ContainerInput>
-          <input type="checkbox" {...register('offer')} />
-          <S.Label> Produto em oferta?</S.Label>
-        </S.ContainerInput>
-        <S.ButtonStyle type="submit"> Adicionar Produto </S.ButtonStyle>
-      </form>
+          </div>
+
+          <S.ContainerInput>
+            <input type="checkbox" {...register('offer')} />
+            <S.Label> Produto em oferta?</S.Label>
+          </S.ContainerInput>
+          <S.ButtonStyle type="submit"> Adicionar Produto </S.ButtonStyle>
+        </form>
+      </Atoms.Box>
     </S.Container>
   )
 }
