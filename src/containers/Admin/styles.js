3efbccaf1daf421a6bf47107467f03bbf9e9fcb7
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-09-20 20:47:13 -0300

    [*] updated

--- src/containers/Admin/styles.js
+++ src/containers/Admin/styles.js
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
-  height: 100%;
-  width: 100%;
+  min-height: 100vh;
+  min-width: 100%;
   background-color: #efefef;
   justify-content: flex-start;
 `
@@ -11,4 +11,5 @@ export const Container = styled.div`
 export const ContainerItems = styled.div`
   padding: 20px;
   width: 80%;
+  min-height: 100vh;
 `
