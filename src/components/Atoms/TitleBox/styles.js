bfead0eabeea740b6f47d5ba620409538acd89a1
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-09-21 08:13:39 -0300

    [+] added title component

---
+++ src/components/Atoms/TitleBox/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Container = styled.h2`
+  font-family: 'Roboto';
+  font-style: normal;
+  margin-bottom: 23px;
+  font-weight: 600;
+  font-size: 15px;
+  line-height: 150%;
+  color: #000;
+`
